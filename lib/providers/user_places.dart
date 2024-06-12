a1e5d74edd8ec91555025c7b8fbab1a8e1af8398
oandeliz <59943720+oandeliz@users.noreply.github.com>
2024-06-11 21:02:13 -0400

    added data bases packages

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,13 +1,20 @@
 import 'dart:io';
 
 import 'package:ff_places/models/places.dart';
+import 'package:path_provider/path_provider.dart' as syspath;
+import 'package:path/path.dart' as path;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title, File image, PlaceLocation location) {
-    final newPlace = Place(title: title, image: image, location: location);
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final appDir = await syspath.getApplicationCacheDirectory();
+    final filename = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$filename');
+
+    final newPlace =
+        Place(title: title, image: copiedImage, location: location);
     state = [newPlace, ...state];
   }
 }
