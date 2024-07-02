39d5edc279e7bdb8be506b7d7bf9fe6949fb7682
pt <petetandon+github@gmail.com>
2024-07-02 19:12:46 -0400

    bugfix: `onBackground` is deprecated

--- lib/screens/add_places.dart
+++ lib/screens/add_places.dart
@@ -57,7 +57,7 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               decoration: const InputDecoration(labelText: 'Title'),
               controller: _titleController,
               style:
-                  TextStyle(color: Theme.of(context).colorScheme.onBackground),
+                  TextStyle(color: Theme.of(context).colorScheme.onSurface),
             ),
             const SizedBox(
               height: 10,
